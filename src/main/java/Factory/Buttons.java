a9f342665917935a33fc2afbece0cba38c3aa645
TeodoraDocheva <teodorakoleva3@gmail.com>
2024-04-18 16:49:12 +0300

    update

---
+++ src/main/java/Factory/Buttons.java
@@ -0,0 +1,45 @@
+package Factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.Duration;
+import java.util.List;
+
+
+public class Buttons {
+    private final WebDriver webDriver;
+
+    @FindBy(xpath = "//i[contains(@class, 'far fa-heart fa-2x')]")
+    private WebElement likeButton;
+
+    @FindBy(xpath = "//i[contains(@class, 'far') and contains(@class, 'fa-heart') and contains(@class, 'liked')]")
+    private WebElement likedButtonState;
+
+    public Buttons(WebDriver driver) {
+        this.webDriver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickLikeButtonOnMostRecentPost() {
+
+        WebDriverWait wait = new WebDriverWait(this.webDriver, Duration.ofSeconds(15));
+
+        try {
+            Thread.sleep(1000); // Sleep for 1 second
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        wait.until(ExpectedConditions.elementToBeClickable(likeButton));
+        likeButton.click();
+    }
+}
+
+
---
+++ src/test/java/WebTesting/LikePostTest.java
@@ -0,0 +1,36 @@
+package WebTesting;
+
+import Factory.*;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class LikePostTest extends TestObject {
+
+    @DataProvider(name = "getUser")
+    public Object[][] getUsers() {
+        return new Object[][]{
+                {"t@k.com", "t@k.com"},
+        };
+    }
+    @Test(dataProvider = "getUser")
+    public void LikeButton(String username, String password) {
+
+        HomePage homePage = new HomePage(webDriver);
+        Header header = new Header(webDriver);
+        LoginPage loginPage = new LoginPage(webDriver);
+        ToastContainer toastContainer = new ToastContainer(webDriver);
+        Buttons buttons = new Buttons(webDriver);
+
+        homePage.navigateTo();
+        header.clickLogin();
+        loginPage.completeSingIn(username, password);
+        buttons.clickLikeButtonOnMostRecentPost();
+        String toastMessage = toastContainer.getToastMessage();
+        Assert.assertEquals(toastMessage, "Post liked");
+
+    }
+}
+
